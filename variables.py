385fbbde8659ec11444e7a26abc665900899b3d3
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2019-09-25 17:02:39 +0000

    i learned today how to create variables in python

---
+++ variables.py
@@ -0,0 +1,26 @@
+# A variable is a container for a value, which can be of various types.
+
+# python file should be start with name of file and .py the end of it.
+'''
+multiline comments
+
+multiline comments in python
+'''
+# declare int
+x = 1
+
+# declare float number
+y = 4.2
+
+# declare string
+name = " hrrat karim" #"or" 
+name_2 = 'gera'
+
+# declare boolan 'True' or 'False'
+is_active = True 
+is_not_active = False
+
+# declare object
+
+
+# declare array
